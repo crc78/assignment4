b22fddd4bc401083fe2e12e579da5c734b96473e
Caitlin Campbell <Caitlin Campbell>
2016-11-29 20:33:50 -0500

    finished assignment

--- assignment4.js
+++ assignment4.js
@@ -10,8 +10,49 @@
 // Add content from requests with something like
 // $.html(), $.text(), etc.
 // keyup events could be helpful to get value of field as the user types
+var valueString;
+var values = [];
+var that = this;
+var list = [];
+
+$(".flexsearch-input").keyup(function(){
+	if($(".flexsearch-input").val() == "" || $(".flexsearch-input").val() == null){
+		list = [];
+		$(".results").html(function(){
+			return "";
+		});
+	}
+	else{
+		list = [];
+		theInput = $(".flexsearch-input").val().toLowerCase();
+
+		values.forEach(function(value){
+			if(value.toLowerCase().match(theInput)){
+				list.push(value);
+			}
+		});
+
+		$(".results").html(function(){
+			let formattedList = "";
+			list.forEach(function(item){
+				formattedList = formattedList + item + "<br>";
+			});
+			return formattedList;
+		});
+	}
+});
 
 (function() {
   // Magic!
   console.log('Keepin\'n it clean with an external script!');
+  $.ajax({url: "http://www.mattbowytz.com/simple_api.json?data=all", success: function(result){
+
+  	for( var prop in result.data.interests){
+  		values.push(result.data.interests[prop]);
+  	}
+
+  	for(var prop in result.data.programming){
+  		values.push(result.data.programming[prop]);
+  	}
+  }});
 })();
